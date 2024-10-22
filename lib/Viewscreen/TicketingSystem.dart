49d3fd818430b0f9b2190d5dd204152d4aa3903f
Moinak Dey <moinakdey21@gmail.com>
2024-10-23 02:27:34 +0530

    TicketingSystem

--- lib/Viewscreen/TicketingSystem.dart
+++ lib/Viewscreen/TicketingSystem.dart
@@ -78,7 +78,69 @@ class _TicketingSystemState extends State<TicketingSystem> {
                     )),
               ),
             ],
+          ),
+          const Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Text("Attractions",style: TextStyle(fontSize: 20),),
+          ),
+          Row(children: [
+             Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: ElevatedButton(
+                    onPressed: () {}, child:  const Column(
+                      children: [Icon(Icons.park_sharp),
+                      Text("Parks")],
+                    )
+                    ),
+              ),
+               Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ElevatedButton(
+                    onPressed: () {}, child:  const Column(
+                      children: [Icon(Icons.category),
+                      Text("Zoo")],
+                    )),
+              ),
+               Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                child: ElevatedButton(
+                    onPressed: () {}, child:  const Column(
+                      children: [Icon(Icons.theaters),
+                      Text("Theatre")],
+                    )),
+                    
+              )
+          ],),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 10),
+                child: ElevatedButton(
+                    onPressed: () {}, child:  const Column(
+                      children: [Icon(Icons.museum),
+                      Text("Museums")],
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                child: ElevatedButton(
+                    onPressed: () {}, child:  const Column(
+                      children: [Icon(Icons.stadium),
+                      Text("Stadiums")],
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                child: ElevatedButton(
+                    onPressed: () {}, child:  const Column(
+                      children: [Icon(Icons.fort),
+                      Text("Sites")],
+                    )),
+              ),
+            ],
           )
+               
+               
         ],
       ),
     );
