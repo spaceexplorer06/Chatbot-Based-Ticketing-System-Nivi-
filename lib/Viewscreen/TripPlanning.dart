833ac58f67722af088b564024955eef14ea6263f
Moinak Dey <moinakdey21@gmail.com>
2024-10-23 08:29:27 +0530

    TripPlanning

--- lib/Viewscreen/TripPlanning.dart
+++ lib/Viewscreen/TripPlanning.dart
@@ -17,7 +17,123 @@ class _TripPlanningState extends State<TripPlanning> {
           padding: EdgeInsets.symmetric(horizontal: 25),
           child: Text("Trip Planning"),
         ),
-      )
+      ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Plan your trip",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Choose a trip type to start planning",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Container(
+              height: 100,
+              width: 340,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(35),
+                  image: const DecorationImage(
+                      image: AssetImage("assets/Images/solo_trip.jpg"),
+                      fit: BoxFit.cover)),
+              child: const Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 5),
+                    child: Text("Solo Trip",
+                        style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 90),
+                    child: Icon(Icons.arrow_right_alt_rounded),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Container(
+              height: 100,
+              width: 340,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(35),
+                  image: const DecorationImage(
+                      image: AssetImage("assets/Images/family_trip.jpg"),
+                      fit: BoxFit.cover)),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 25, horizontal: 5),
+                child: Row(
+                  children: [
+                    Text(
+                      "Family Trip",
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 70),
+                      child: Icon(Icons.arrow_right_alt),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Container(
+              height: 100,
+              width: 340,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(35),
+                  image: const DecorationImage(
+                      image: AssetImage("assets/Images/collaborative.jpg"),
+                      fit: BoxFit.cover)),
+              child: const Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 5),
+                    child: Text(
+                      "Collab Trip",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 70),
+                    child: Icon(Icons.arrow_right_alt),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
