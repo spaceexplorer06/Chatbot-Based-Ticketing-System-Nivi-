d52e7f9a631065e9384931ac8611682b8155841c
Moinak Dey <moinakdey21@gmail.com>
2024-10-23 06:03:45 +0530

    Budapest and London

--- lib/Home/California.dart
+++ lib/Home/California.dart
@@ -1,4 +1,6 @@
 import 'package:app/Chat.dart';
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:like_button/like_button.dart';
@@ -57,7 +59,111 @@ class _CaliforniaState extends State<California> {
                       color: Colors.black,
                     ))
               ],
-            )
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                "Visits this month : 4.5K",
+                style: TextStyle(fontSize: 25, color: Colors.blue, fontFamily: 'belanosima'),
+              ),
+            ),
+            const Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Text(
+                    "TOP SIGHTS : ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
+            CarouselSlider(
+                items: [
+                  SizedBox(
+                    height: 200,
+                    child: Column(children: [
+                      Container(
+                        height: 200,
+                        width: 300,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          image: const DecorationImage(
+                            image: AssetImage('assets/Images/cali1.png'),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 240,
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ),
+                  SizedBox(
+                    height: 200,
+                    child: Column(children: [
+                      Container(
+                        height: 200,
+                        width: 300,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          image: const DecorationImage(
+                            image: AssetImage('assets/Images/cali2.png'),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 240,
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ),
+                  SizedBox(
+                    height: 200,
+                    child: Column(children: [
+                      Container(
+                        height: 200,
+                        width: 300,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          image: const DecorationImage(
+                            image: AssetImage('assets/Images/cali3.png'),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 240,
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ),
+                ],
+                options: CarouselOptions(
+                  height: 300,
+                  enlargeCenterPage: true,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 3),
+                  aspectRatio: 16 / 9,
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enableInfiniteScroll: true,
+                  autoPlayAnimationDuration: const Duration(milliseconds: 750),
+                  viewportFraction: 0.8,
+                )),
+            const ElevatedButton(
+                onPressed: null,
+                child: Text(
+                  "Up for a visit ?",
+                  style: TextStyle(color: Colors.black),
+                ),
+                style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(
+                        Color.fromARGB(252, 255, 122, 50)))),
           ],
         ),
       ),
