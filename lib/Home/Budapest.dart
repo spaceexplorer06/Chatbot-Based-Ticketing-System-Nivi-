0f7b0c1d974eac5a0d3cb20a257af6d24c9c69d4
Moinak Dey <moinakdey21@gmail.com>
2024-10-23 05:54:57 +0530

    Budapest

--- lib/Home/Budapest.dart
+++ lib/Home/Budapest.dart
@@ -1,4 +1,6 @@
 import 'package:app/Chat.dart';
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:like_button/like_button.dart';
@@ -15,6 +17,7 @@ class _BudapestState extends State<Budapest> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text("Főváros", style: TextStyle(fontWeight: FontWeight.bold),),
         actions: const <Widget>[
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 10),
@@ -55,9 +58,114 @@ class _BudapestState extends State<Budapest> {
                     icon: const Icon(
                       Icons.chat,
                       color: Colors.black,
-                    ))
+                    )
+                    )
               ],
-            )
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                "Visits this month : 6.5K",
+                style: TextStyle(fontSize: 25, color: Colors.blue, fontFamily: 'belanosima'),
+              ),
+            ),
+            const Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Text(
+                    "TOP SIGHTS : ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
+            CarouselSlider(
+                items: [
+                  SizedBox(
+                    height: 200,
+                    child: Column(children: [
+                      Container(
+                        height: 200,
+                        width: 300,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          image: const DecorationImage(
+                            image: AssetImage('assets/Images/buda1.jpg'),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 240,
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ),
+                  SizedBox(
+                    height: 200,
+                    child: Column(children: [
+                      Container(
+                        height: 200,
+                        width: 300,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          image: const DecorationImage(
+                            image: AssetImage('assets/Images/buda2.jpg'),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 240,
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ),
+                  SizedBox(
+                    height: 200,
+                    child: Column(children: [
+                      Container(
+                        height: 200,
+                        width: 300,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          image: const DecorationImage(
+                            image: AssetImage('assets/Images/buda3.jpg'),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 240,
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ),
+                ],
+                options: CarouselOptions(
+                  height: 300,
+                  enlargeCenterPage: true,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 3),
+                  aspectRatio: 16 / 9,
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enableInfiniteScroll: true,
+                  autoPlayAnimationDuration: const Duration(milliseconds: 750),
+                  viewportFraction: 0.8,
+                )),
+            const ElevatedButton(
+                onPressed: null,
+                child: Text(
+                  "Up for a visit ?",
+                  style: TextStyle(color: Colors.black),
+                ),
+                style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(
+                        Color.fromARGB(252, 255, 122, 50)))),
           ],
         ),
       ),
